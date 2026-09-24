fdd68dd9e1df68fae42eced7373bde40cf35996e
João Victor Martins Cid <martins.cid@proton.me>
2026-09-24 18:01:42 -0300

    Add files via upload

---
+++ accounting-service/src/test/java/com/br/axioma/accounting_service/AccountingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.axioma.accounting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
